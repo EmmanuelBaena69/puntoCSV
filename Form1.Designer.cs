579671439657da38934ba8c8ca09693952e8f062
SakuSaku <travispastrana075@gmail.com>
2023-04-27 18:29:36 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace puntoCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblPais = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.lblGuia = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(243, 192);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(86, 36);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(485, 192);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(86, 36);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(362, 192);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(86, 36);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Actualizar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lblPais
+            // 
+            this.lblPais.AutoSize = true;
+            this.lblPais.Location = new System.Drawing.Point(243, 111);
+            this.lblPais.Name = "lblPais";
+            this.lblPais.Size = new System.Drawing.Size(34, 16);
+            this.lblPais.TabIndex = 4;
+            this.lblPais.Text = "Pais";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(243, 71);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 5;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(243, 155);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(40, 16);
+            this.lblEdad.TabIndex = 6;
+            this.lblEdad.Text = "Edad";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(346, 71);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(136, 28);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEdad.Location = new System.Drawing.Point(346, 155);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(136, 28);
+            this.txtEdad.TabIndex = 8;
+            // 
+            // txtPais
+            // 
+            this.txtPais.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPais.Location = new System.Drawing.Point(346, 111);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(136, 28);
+            this.txtPais.TabIndex = 9;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(129, 234);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(535, 204);
+            this.dgv.TabIndex = 10;
+            this.dgv.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgv_CellMouseClick);
+            // 
+            // lblGuia
+            // 
+            this.lblGuia.AutoSize = true;
+            this.lblGuia.Location = new System.Drawing.Point(343, 35);
+            this.lblGuia.Name = "lblGuia";
+            this.lblGuia.Size = new System.Drawing.Size(18, 16);
+            this.lblGuia.TabIndex = 11;
+            this.lblGuia.Text = "id";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(255, 35);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(20, 16);
+            this.lblID.TabIndex = 12;
+            this.lblID.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblGuia);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.txtPais);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblPais);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label lblPais;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label lblGuia;
+        private System.Windows.Forms.Label lblID;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.CodeDom;
+
+namespace puntoCSV
+{
+    public partial class Form1 : Form
+    {
+        string path = "archivo.csv";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(path))
+            {
+                File.Create(path).Dispose();
+                using (TextWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine("id,nombre,pais,edad");
+                }
+            }
+            grid();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(path))
+            {
+                int count = 0;
+                try
+                {
+                    count = Convert.ToInt32(File.ReadAllLines(path).Last().Split(',')[0]);
+                    count = count + 1;
+                }
+                catch (Exception e1)
+                {
+                    count = 1;
+                }
+                File.AppendAllLines(path, new[] {count.ToString()+","+txtName.Text+","+txtPais.Text+","+txtPais.Text});
+            }
+            grid();
+        }
+
+        public void grid()
+        {
+            dgv.DataSource = null;
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("nombre");
+            dt.Columns.Add("Pais");
+            dt.Columns.Add("Edad");
+
+            string[] lineas = File.ReadAllLines(path);
+            lineas = lineas.Skip(1).ToArray();
+
+            foreach (string linea in lineas)
+            {
+                DataRow fila = dt.NewRow();
+                string[] valores = linea.Split(',');
+                int i = 0;
+                foreach (string valor in valores)
+                {
+                    fila[i] = valor;
+                    i++;
+                }
+                dt.Rows.Add(fila);
+            }   
+            dgv.DataSource= dt;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string[] lineas = File.ReadAllLines(path);
+            int j = 0;
+            foreach (string linea in lineas) 
+            {
+                if (linea.Split(',')[0] == lblGuia.Text)
+                {
+                    break;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+            lineas[j]= lineas[j].Split(',')[0] + "," + txtName.Text + "," + txtPais.Text + "," + txtEdad.Text;
+            File.WriteAllLines(path, lineas);
+            grid();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string[] lineas = File.ReadAllLines (path);
+            int j = 0;
+            foreach (string linea in lineas)
+            {
+                if (linea.Split(',')[0]==lblGuia.Text) 
+                { 
+                    break;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+            string[] newlinea = lineas.Where(g => !g.Equals(lineas[j])).ToArray();
+            File.WriteAllLines(path, newlinea);
+            grid();
+        }
+
+        private void dgv_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow columnas = dgv.Rows[e.RowIndex];
+                lblGuia.Text = columnas.Cells[0].Value.ToString();
+            }
+        }
+    }
+}
